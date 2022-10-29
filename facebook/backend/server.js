c714f721262b05383559ffaf5f8bee49cf6b8685
Arya Singh <aryasinghbjc@gmail.com>
2022-10-29 23:12:45 +0530

    Adds Database connection

--- facebook/backend/server.js
+++ facebook/backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { readdirSync } = require("fs");
@@ -7,7 +8,15 @@ const app = express();
 dotenv.config();
 app.use(cors());
 
+//routes
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
+
+//database
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Database connection established"))
+  .catch((err) => console.log(`Error connecting to database ${err}`));
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
